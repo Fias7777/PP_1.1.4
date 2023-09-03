9c288aab7217f180b100fb8a7df1a8c2b0e0e5e7
Fias7777 <102873032+Fias7777@users.noreply.github.com>
2023-09-03 04:55:20 +0300

    add Override

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Gon", "Frick", (byte) 12);
         userService.saveUser("killua", "Zoldik", (byte) 13);
